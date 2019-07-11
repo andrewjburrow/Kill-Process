20c7daada14d71ace96b79463c22939d807e1bc2
ψ S̸̟̦̱͖͋̋̔͛͌u̸̐̀ <52605755+andrewjburrow@users.noreply.github.com>
2019-07-11 23:48:36 +0430

    changes

--- Kill Process.py
+++ Kill Process.py
@@ -1,35 +1,68 @@
 import psutil
 
 
+
 processName = input('Name> ')
 def checkIfProcessRunning(processName):
 
     for proc in psutil.process_iter():
         try:
 
-            if processName.lower() in proc.name().lower():#idk why this works
+            if processName.lower() in proc.name().lower():
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    return False; #3 hours to figure out why this didnt work
+    return False;
 
 def star(processName):#true or false
     if checkIfProcessRunning(processName) == True:
         print("Found the Process")
-        input("KILL [ENTER]")#holds script for user interaction
-        killer(processName)
+
+        #holds script for user interaction
+        pid_find(processName)
     elif checkIfProcessRunning(processName) == False:
         print("Nothing Similar Running")
 
 
-def killer(processName):
+def pid_find(processName):
+    pid = []
+    for proc in psutil.process_iter():
+        try:
+            if proc.name() == processName+'.exe':
+                pid.append(proc.pid)
+        except psutil.AccessDenied:
+            pass
     try:
-        for proc in psutil.process_iter():#initiate process_iter
-            proc.name = processName
-            if proc.name == processName:#if equal to corresponding name
-                p = psutil.Process(proc.pid)
-                p.kill()
-    except:
-        print("cannot kill process")
+        process = pid[0]
+    except IndexError:
+        print("no pid")
+        quit()
+     #print process id
+    killer(processName, process)
+
+def killer(processName, process):# Need to use psutil instead of os library
+    counterP = []
+    counterC = []
+    parent_pid = process
+    parent = psutil.Process(process)
+    for child in parent.children(recursive=True):
+        counterC.append(child)
+        child.kill()
+    counterP.append(parent)
+    parent.kill()
+    if checkIfProcessRunning(processName) == True:
+        star(processName)
+    elif checkIfProcessRunning(processName) == False:
+        print('{Child Process}')
+        print('-'*25)
+        for x in counterC:
+            print(x)
+        print('-'*25)
+        print('{Parent Process}')
+        for x in counterP:
+            print(x)
+        print('-'*25)
+
+
 
 star(processName)
